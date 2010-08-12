a75a3ae6dfc4ea83935a0b30f26512d4ef228729
Dirk Brünsicke <d1rk@gmx.de>
2010-08-12 21:40:52 +0200

    removed unused method

--- models/content.php
+++ models/content.php
@@ -31,51 +31,5 @@ class Content extends FlourAppModel
 		),
 	);
 
-
-/**
- * binds the model, associated via model and foreign_id.
- *
- * @param array $data 
- * @return bool
- * @access public
- */
-	function _bind($data = array())
-	{
-		$this->data = (!empty($data))
-			? $data
-			: $this->data;
-
-		if(isset($this->data['Content']['model']))
-		{
-			$model = (isset($this->data['Content']['model']))
-				? $this->data['Content']['model']
-				: 'Flour.ContentObject';
-
-			$foreignModel = ClassRegistry::init($model);
-
-			$this->bindModel(
-				array('belongsTo' => array(
-						$foreignModel->alias => array(
-							'className' => $model,
-							// 'conditions' => array('model' => $model),
-							'foreignKey' => 'foreign_id',
-						)
-					)
-				)
-			);
-			$this->{$foreignModel->alias}->bindModel(
-				array('hasOne' => array(
-					'Content' => array(
-						'className' => 'Flour.Content',
-						'foreignKey' => 'foreign_id',
-						'dependant' => true,
-					)
-				))
-			);
-			$this->{$foreignModel->alias}->data = $this->data;
-			return true;
-		}
-		return false;
-	}
 }
 ?>
