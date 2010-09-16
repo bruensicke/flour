4b08174cf609d9c1f534c3a8fd64577a23ce4bc9
Dirk Brünsicke <d1rk@gmx.de>
2010-09-16 20:24:08 +0200

    added flour default routes, thus creating a dashboard, although empty

---
+++ controllers/flour_controller.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * FlourController
+ * 
+ * Shows Documentation of Flour.
+ *
+ * @package flour
+ * @author Dirk Brünsicke
+ * @version $Id$
+ * @copyright bruensicke.com GmbH
+ **/
+class FlourController extends FlourAppController
+{
+
+	var $uses = null;
+
+/**
+ * Index action.
+ *
+ * @access public
+ */
+	function admin_index()
+	{
+
+	}
+
+}
+?>
