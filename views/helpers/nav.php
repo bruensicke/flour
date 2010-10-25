5fee323a87f09cb5171aa00c7c433cfb9df3585e
Dirk Brünsicke <d1rk@gmx.de>
2010-10-25 21:44:48 +0200

    adding local data handling for NavHelper

--- views/helpers/nav.php
+++ views/helpers/nav.php
@@ -38,7 +38,37 @@ class NavHelper extends AppHelper
  * @access protected
  */
 	protected $_View = false;
-	
+
+
+/**
+ * used for internal handling of complete Navigations
+ *
+ * @var array
+ * @access public
+ */
+	public $_cache = array();
+
+	public $navigationTemplate = array(
+		'Navigation' => array(
+			'type' => 'default',
+			'name' => '',
+			'class' => '',
+			'template' => 'default',
+			'status' => 1,
+			'slug' => '',
+		),
+		'NavigationItem' => array(),
+	);
+
+	public $navigationItemTemplate = array(
+		'NavigationItem' => array(
+			'type' => 'default',
+			'template' => 'default',
+			'status' => 1,
+			'url' => '',
+		),
+	);
+
 /**
  * Constructor method
  *
@@ -58,7 +88,147 @@ class NavHelper extends AppHelper
  */
 	public function get($slug_or_id)
 	{
+		if(array_key_exists($slug_or_id, $this->_cache))
+		{
+			return $this->_cache[$slug_or_id];
+		}
 		return $this->_Navigation->get($slug_or_id);
+	}
+
+/**
+ * renders the current Navigation with given $slug
+ *
+ * @param string | array $slug_or_id give slug of Navigation (from DB) or pass in data to render
+ * @param string $template name of template 
+ * @param array $data array with data to be passed into the element
+ * @return string $output the HTML rendered by the element
+ * @access public
+ */
+	public function render($slug_or_data, $template = 'navigations/item', $data = array())
+	{
+		$row_data = (is_array($slug_or_data))
+			? $slug_or_data
+			: $this->get($slug_or_data);
+
+		//if second param is array, it is data (third param will be omitted)
+		$data = (is_array($template))
+			? $template
+			: $data;
+
+		$data = array_merge($data, array('row' => $row_data));
+		return $this->_View->element($template, $data);
+	}
+
+/**
+ * Create a Navigation Container with given $slug and $data, also accepts $rows to insert NavigationItems
+ * 
+ * {{{
+ * $this->Nav->create('primary');
+ * $this->Nav->create('primary', array('name' => 'Toplevel Navigation', 'template' => 'short'));
+ * }}}
+ *
+ * @param string $slug the slug of the Navigation
+ * @param array $data to be set for the Navigation
+ * @param array $rows a list of NavigationItems, as returned by a Navigation->find
+ * @return array the new generated version of the Navigation
+ * @access public
+ */
+	public function create($slug, $data = array(), $rows = array())
+	{
+		$this->_createLocal($slug, $data);
+
+		if(!empty($rows))
+		{
+			foreach($rows as $row)
+			{
+				$this->add($slug, $row);
+			}
+		}
+		return $this->_cache[$slug];
+	}
+
+	//if $name is array, it is used as $options, $url is omitted
+
+/**
+ * adds a NavigationItem to Navigation with slug $slug
+ *
+ * @param string $slug name of this Navigation
+ * @param string|array if string, $name of the link, if is_array, it behaves like $data
+ * @param string|array $url to be used as url (can be an array or null)
+ * @param array $data use all fields available in NavigationItem schema
+ * @return array full array of Navigation including NavigationItem
+ * @todo should also work with $rows of NavigationItems
+ * @access public
+ */
+	public function add($slug, $name = null, $url = null, $data = array())
+	{
+		//consolidate input-params
+		$data = (is_array($name))
+			? Set::merge($data, $name)
+			: Set::merge($data, array('name' => $name, 'url' => $url));
+
+		$this->_createLocal($slug);
+
+		return $this->_addLocal($slug, $data);
+	}
+
+
+/* internal methods */
+
+/**
+ * Create an internal, cached version of a Navigation, named with $slug
+ *
+ * @param string|array $slug if string, $slug for Navigation, if array, it behaves like $data (second param is omitted)
+ * @param array $data array with keys named as found in the Navigation Schema
+ * @return array the new generated version of the Navigation
+ * @access public
+ */
+	public function _createLocal($slug, $data = array())
+	{
+		if(is_array($slug))
+		{
+			$data = $this->_normalizeRow($slug);
+			$slug = (!empty($data['Navigation']['slug']))
+				? $data['Navigation']['slug']
+				: Inflector::slug($data['Navigation']['name']);
+		} else {
+			$data = $this->_normalizeRow($data);
+		}
+
+		if(!array_key_exists($slug, $this->_cache))
+		{
+			$this->_cache[$slug] = Set::merge($this->navigationTemplate, $data);
+		}
+		return $this->_cache[$slug];
+	}
+
+/**
+ * Add NavigationItems to an internal, cached version named $slug
+ *
+ * @param string $slug 
+ * @param array $data 
+ * @return bool
+ * @access public
+ */
+	public function _addLocal($slug, $data = array())
+	{
+		$this->_createLocal($slug);
+		$this->_cache[$slug]['NavigationItem'][] = Set::merge($this->navigationItemTemplate['NavigationItem'], $data);
+		return $this->_cache[$slug];
+	}
+
+
+/**
+ * Make sure we have the modelname as key
+ *
+ * @param array $row
+ * @return array with "$key" key
+ */
+	function _normalizeRow($row, $key = 'Navigation')
+	{
+		if (!is_array($row)) return null;
+ 		if (array_key_exists($key, $row)) return $row;
+		return array($key => $row);
 	}
 
 /**
