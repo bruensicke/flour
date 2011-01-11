f9d1bbb8257b3cff0b4bb28fdac8a8d4827a5fd9
sassman <sven.assmann@lubico.biz>
2011-01-11 15:54:41 +0100

    fix bits on user accounting models

--- models/account.php
+++ models/account.php
@@ -1,12 +1,9 @@
 <?php
 /**
- * Account Membership Model
- *
- * You can use this model or copy it to your app.
+ * Account Model
  *
  * @package flour
  * @author Dirk Brünsicke
- * @version $Id$
  * @copyright brünsicke.com GmbH
  **/
 class Account extends FlourAppModel  {
@@ -18,11 +15,6 @@ class Account extends FlourAppModel  {
 		'status' => array(
 			'numeric' => array(
 				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
 	);
--- models/user.php
+++ models/user.php
@@ -34,13 +34,13 @@ class User extends FlourAppModel
 		$token = md5(uniqid(mt_rand(), true));
 		$userId = $user['User']['id'];
 
-		$this->LoginToken->create(array(
-			'user_id' => $userId,
-			'token' => $token,
-			'duration' => $duration,
-			'expires' => date('Y-m-d H:i:s', strtotime($duration)),
-		));
-		$this->LoginToken->save();
+// 		$this->LoginToken->create(array(
+// 			'user_id' => $userId,
+// 			'token' => $token,
+// 			'duration' => $duration,
+// 			'expires' => date('Y-m-d H:i:s', strtotime($duration)),
+// 		));
+// 		$this->LoginToken->save();
 
 		return "${token}:${userId}";
 	}
@@ -67,23 +67,23 @@ class User extends FlourAppModel
 				list($token, $userId) = split(':', $credentials['token']);
 				$duration = $credentials['duration'];
 
-				$loginToken = $this->LoginToken->find('first', array(
-					'conditions' => array(
-					'user_id' => $userId,
-					'token' => $token,
-					'duration' => $duration,
-					'used' => false,
-					'expires <=' => date('Y-m-d H:i:s', strtotime($duration)),
-				),
-				'contain' => false
-				));
+// 				$loginToken = $this->LoginToken->find('first', array(
+// 					'conditions' => array(
+// 						'user_id' => $userId,
+// 						'token' => $token,
+// 						'duration' => $duration,
+// 						'used' => false,
+// 						'expires <=' => date('Y-m-d H:i:s', strtotime($duration)),
+// 					),
+// 					'contain' => false
+// 				));
 
 				if (!$loginToken) {
 					return false;
 				}
 
 				$loginToken['LoginToken']['used'] = true;
-				$this->LoginToken->save($loginToken);
+// 				$this->LoginToken->save($loginToken);
 
 				$conditions = array(
 					'User.id' => $loginToken['LoginToken']['user_id']
@@ -99,6 +99,4 @@ class User extends FlourAppModel
 		$user['User']['loginType'] = $type;
 		return $user;
 	}
-	
 }
-?>
