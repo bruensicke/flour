cf1b8b547e76fbdd3cd681b3f89638da3a159980
d1rk <d1rk@gmx.de>
2011-02-08 22:25:09 +0100

    adding Template Model

---
+++ models/template.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Template Model
+ * 
+ * a Template is just some markup
+ * 
+ * @package flour
+ * @author Dirk Brünsicke
+ * @copyright bruensicke.com GmbH
+ **/
+class Template extends FlourAppModel
+{
+
+/**
+ * Attached behaviors
+ *
+ * @var array
+ * @access public
+ */
+	public $actsAs = array(
+		'Flour.Taggable',
+	);
+
+/**
+ * Validation rules
+ *
+ * @var array
+ * @access public
+ */
+	public $validate = array(
+		'type' => array(
+			'notEmpty' => array('rule' => 'notEmpty', 'required' => true),
+		),
+		'name' => array(
+			'notEmpty' => array('rule' => 'notEmpty', 'required' => true),
+		),
+		'slug' => array(
+			'notEmpty' => array('rule' => 'notEmpty', 'required' => true),
+		),
+	);
+
+/**
+ * gets the current Template with given $slug
+ *
+ * @param string $slug slug of Template to retrieve (or id)
+ * @return mixed array of $data, if found in database for the currently active content - false otherwise
+ * @access public
+ */
+	public function get($slug_or_id)
+	{
+		$field = (Validation::uuid($slug_or_id))
+			? 'id'
+			: 'slug';
+
+		$data = $this->find('current', array($field => $slug_or_id));
+		if(empty($data))
+		{
+			return false;
+		}
+		return $data;
+	}
+
+}
