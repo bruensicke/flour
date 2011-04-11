e9c47a12d7c85f96c731a0a2d6c7be2fd503ad3a
d1rk <d1rk@gmx.de>
2011-04-11 10:41:59 +0200

    allow specificed extensions to be autoswitched to Webservice-View

--- controllers/components/layout.php
+++ controllers/components/layout.php
@@ -45,6 +45,24 @@ class LayoutComponent extends Object
 	);
 
 	/**
+	 * All Extensions that are automatically switched to Webservice View
+	 *
+	 * set to an empty to array, to turn off this feature
+	 * Hint: You must turn put this into your routes.php
+	 *
+	 * {{{
+	 *  Router::parseExtensions('xml', 'json');
+	 * }}}
+	 *
+	 * @var array list of extensions to deliver with webservice
+	 * @access public
+	 */
+	public $webserviceExtionsions = array(
+		'json',
+		'xml',
+	);
+
+	/**
 	 * Instance of calling Controller
 	 *
 	 * @var object Controller
@@ -81,10 +99,14 @@ class LayoutComponent extends Object
 	function setup($settings = array())
 	{
 		if(!empty($settings)) $this->settings = $settings;
-		
+
 		$prefix = (!empty($this->__controller->params['prefix']))
 			? $this->__controller->params['prefix']
 			: 'default';
+
+		$ext = (!empty($this->__controller->params['url']['ext']))
+			? $this->__controller->params['url']['ext']
+			: null;
 
 		$theme = (!empty($this->settings['themes'][$prefix]))
 			? $this->settings['themes'][$prefix]
@@ -94,13 +116,18 @@ class LayoutComponent extends Object
 			? $this->settings['view']
 			: 'Theme';
 
+		//change to Webservice-View if current Extension is one of $this->webserviceExtionsions
+		if(in_array($ext, $this->webserviceExtionsions)) {
+			$view = 'Flour.Webservice';
+		}
+
 		$layout = String::insert($this->settings['layout'], compact('prefix'));
 
 		$this->__controller->view = $view;
 		$this->__controller->theme = $theme;
 		$this->__controller->layout = $layout;
 
-		Configure::write('App.Layout.current', compact('prefix', 'theme', 'view', 'layout'));
+		Configure::write('App.Layout.current', compact('prefix', 'theme', 'view', 'layout', 'ext'));
 	}
 
 }
